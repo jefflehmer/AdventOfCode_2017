48eb20dfc26318d1e846f3cdb90f711d742dd4f8
kendodragon <kendo_dragon@yahoo.com>
2017-12-21 21:28:49 -0500

    Day 17.1

---
+++ aoc_2017/Day17/Day_17.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2017.Day17
+{
+    class Day_17
+    {
+        public static void Do()
+        {
+            var NumInsertions = 2018;
+
+            var spinlock = new SpinLock();
+            for (int i = 1; i < NumInsertions; i++) // 0 added in SpinLock ctor
+                spinlock.Insert(i);
+
+            Console.WriteLine($"Day 17: Value after { NumInsertions } insertions: { spinlock.After }");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ aoc_2017/Day17/SpinLock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2017.Day17
+{
+    class SpinLock
+    {
+        private LinkedListNode<int> CurrentNode { get; set; }
+
+        public LinkedList<int> List { get; set; }
+        //int NumSteps = 3; // test
+        int NumSteps = 337; // actual
+
+        // returns the value immediately _after_ the current position
+        public int After => Next().Value;
+        public int AfterFirst => List.First.Next.Value;
+
+        public SpinLock()
+        {
+            List = new LinkedList<int>();
+            CurrentNode = List.AddFirst(0);
+        }
+
+        internal void Insert(int i)
+        {
+            CurrentNode = Advance().AddAfter(CurrentNode, i);
+        }
+
+        internal LinkedList<int> Advance()
+        {
+            for (var j = 0; j < NumSteps; j++)
+                CurrentNode = Next();
+
+            return List;
+        }
+
+        private LinkedListNode<int> Next()
+        {
+            return CurrentNode == List.Last ? List.First : CurrentNode.Next;
+        }
+    }
+}
